3cb09eb68406b54f4321079c86752d5e11d1be07
yura901 <yura901@ukr.net>
2017-08-01 19:23:03 +0300

    first commit

---
+++ lndex.php
@@ -0,0 +1,58 @@
+<?php
+    $name = "yura";
+    echo  $name;
+    $age =  "30\n";
+    echo $age;
+    echo "Меня зовут Юра\n";
+    echo "Мне 30лет\n";
+
+    $risunki = "80";
+    $flomaster = "26";
+    $karandasi = "40";
+    echo $kraski = $risunki - ($flomaster + $karandasi);
+
+    $age = 25;
+    if ($age == 18-35) {
+        echo 'Вам еще работать и работать';
+    }
+    if ($age > 65) {
+        echo "Вам пора на пенсию";
+    }
+
+   // define (value ("pi") == 5);
+    //if (defined ("pi")
+        //echo "pi";//
+
+    $day = 1;
+switch ($day) {
+    case 1:
+        echo "Это рабочий день"; break;
+
+}
+
+    $day = 6;
+    $day = 7;
+        switch ($day) {
+        case 6:
+        case 7;
+        echo "Это выходной день"; break;
+
+}
+    $day = 10;
+    switch ($day) {
+        case 6:
+        case 7;
+        echo "Это неизвестный день"; break;
+
+}   $car = ["car bmw"];
+    echo $car[0];
+    $bmw = ["X5", "120", "5", "215"];
+    echo $bmw[0];
+    echo $bmw[1];
+    echo $bmw[2];
+    echo $bmw[3];
+
+
+
+
+?>
