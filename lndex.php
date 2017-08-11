73d430ee355bdda548e6363655dbae59ab6741ca
yura901 <yura901@ukr.net>
2017-08-11 14:46:48 +0300

    new commit

--- lndex.php
+++ lndex.php
@@ -1,32 +1,35 @@
 <?php
     $name = "yura";
-    echo  $name;
-    $age =  "30\n";
-    echo $age;
-    echo "Меня зовут Юра\n";
-    echo "Мне 30лет\n";
+    echo  $name. '<br>';
+    $age =  "30";
+    echo $age. '<br>';
+    echo "'Меня зовут $name. '<br>'";
+    echo "'Мне $age. лет '<br>'";// Задание 1
 
     $risunki = "80";
     $flomaster = "26";
     $karandasi = "40";
-    echo $kraski = $risunki - ($flomaster + $karandasi);
+    echo $kraski = $risunki - ($flomaster + $karandasi);//Задание 2
+
+    define('const', '30', true);
+    echo 'const'.'<br>';
 
     $age = 25;
-    if ($age == 18-35) {
-        echo 'Вам еще работать и работать';
+    if ($age >18-35) {
+        echo 'Вам еще работать и работать'. '<br>';
     }
-    if ($age > 65) {
-        echo "Вам пора на пенсию";
+    elseif ($age > 65) {
+        echo 'Вам пора на пенсию'. '<br>';
     }
 
    // define (value ("pi") == 5);
     //if (defined ("pi")
         //echo "pi";//
 
-    $day = 1;
-switch ($day) {
+    $day = 1;//Задание 5
+    switch ($day) {
     case 1:
-        echo "Это рабочий день"; break;
+        echo "Это рабочий день". '<br>'; break;
 
 }
 
@@ -42,15 +45,38 @@ switch ($day) {
     switch ($day) {
         case 6:
         case 7;
-        echo "Это неизвестный день"; break;
+        echo "Это неизвестный день".'<br>'; break;//Задание 5
 
-}   $car = ["car bmw"];
-    echo $car[0];
-    $bmw = ["X5", "120", "5", "215"];
-    echo $bmw[0];
-    echo $bmw[1];
-    echo $bmw[2];
-    echo $bmw[3];
+}
+
+
+    $bmw = ['model', 'speed', 'doors', 'year'];//хадание 6
+
+
+    echo '<pre>';
+    print_r($bmw) .'<br>';
+
+
+    $a = 1;
+    $b = 2;
+    $c = 3;
+    $d = 4;
+    $e = 5;
+    $g = 6;
+    $i = 7;
+    $f = 8;
+    $k = 9;
+    $l = 10;
+    $r = $a * $b;
+    $r1 = $a * $c;
+    $r2 = $a * $d;
+    $r3 = $a * $e;
+    $r4 = $a * $i;
+    $r5 = $a * $f;
+    $r6 = $a * $k;
+    $r7 = $a * $l;
+    echo "$r<br> $r1<br> $r2<br> $r3<br> $r4<br> $r5<br> $r6<br> $r7<br>";
+
 
 
 
